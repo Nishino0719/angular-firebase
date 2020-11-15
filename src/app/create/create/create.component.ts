968bc3f0d7d9f92f9b0fc36b62c7bdd4d7be0631
Nishino0719 <nishino0719@icloud.com>
2020-11-15 16:04:30 +0900

    feat(pet): can send pet data to firestore

--- src/app/create/create/create.component.ts
+++ src/app/create/create/create.component.ts
@@ -2,6 +2,9 @@ import { compileNgModule } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import { Pet } from 'src/app/interfaces/pet';
+import { AuthService } from 'src/app/services/auth.service';
+import { PetService } from 'src/app/services/pet.service';
 
 @Component({
   selector: 'app-create',
@@ -18,7 +21,7 @@ export class CreateComponent implements OnInit {
       el: '.pager',
       clickable: true
     },
-    centeredSlides : true,
+    centeredSlides: true,
     slidesPerView: 3
   };
   selectedPetId = 0;
@@ -38,15 +41,26 @@ export class CreateComponent implements OnInit {
   }
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private petService: PetService,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
   }
 
-  submit(): void {
+  submit(): any {
+    const formData = this.form.value;
     console.log(this.form.value);
     console.log(this.selectedPetId);
+    this.petService.createPet({
+      petImageId: this.selectedPetId,
+      name: this.form.value.name,
+      level: 1,
+      exp: 0,
+      trainerId: this.authService.uid,
+      gender: this.form.value.gender
+    });
   }
 
 }
